b020608d74efa71bd8561bb7a3470a8cfec22d2e
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-04-07 19:18:19 +0630

    updates for minimize_maximum_lateness

--- Greedy Algorithms/minimize_lateness.py
+++ Greedy Algorithms/minimize_lateness.py
@@ -2,21 +2,17 @@ def minimize_lateness(tasks):
     sorted_tasks = sorted(tasks, key=lambda task: task[1])
 
     current_time = 0
-    lateness = [0] * len(tasks)
-    task_order = [0] * len(tasks)
     max_lateness = 0
+    task_order = []
 
-    for i in range(len(sorted_tasks)):
-        task = sorted_tasks[i]
-        index = tasks.index(task)
-        task_order[i] = index
-        current_time += task[0]
-        lateness[i] = max(0, current_time - task[1])
-        max_lateness = max(max_lateness, lateness[i])
+    for task in sorted_tasks:
+        duration, deadline = task
+        current_time += duration
+        lateness = max(current_time - deadline, 0)
+        max_lateness = max(max_lateness, lateness)
+        task_order.append(tasks.index(task))
 
     return task_order, max_lateness
-
-
 
 # Read input
 n = int(input())
